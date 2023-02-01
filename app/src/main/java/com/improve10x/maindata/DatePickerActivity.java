1f539d1e0827a51edb5aadfe1cea4c784694cb09
Manohar <manohar.mangali@improve10x.com>
2023-02-01 12:57:01 +0530

    I am added leap year

--- app/src/main/java/com/improve10x/maindata/DatePickerActivity.java
+++ app/src/main/java/com/improve10x/maindata/DatePickerActivity.java
@@ -12,6 +12,7 @@ public class DatePickerActivity extends AppCompatActivity {
     TextView currentTxt;
     DatePicker pickerDp;
     Button changeDateBtn;
+    TextView yearTxt;
 
 
     @Override
@@ -20,6 +21,7 @@ public class DatePickerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_date_picker);
         currentTxt = findViewById(R.id.current_txt);
         pickerDp = findViewById(R.id.date_picker_dp);
+        yearTxt = findViewById(R.id.year_txt);
         changeDateBtn = findViewById(R.id.change_date_btn);
         displayCurrentDate();
         displayChangeDate();
@@ -28,27 +30,36 @@ public class DatePickerActivity extends AppCompatActivity {
     private void displayChangeDate() {
         String selectedDate = getSelectedDate();
         currentTxt.setText("CurrentDate :" + selectedDate);
-
     }
 
     private void displayCurrentDate() {
-      changeDateBtn.setOnClickListener(v -> {
-          String selectedDate = getSelectedDate();
-          currentTxt .setText("ChangeDate :" + selectedDate);
-
-      });
+        changeDateBtn.setOnClickListener(v -> {
+            String selectedDate = getSelectedDate();
+            currentTxt.setText("ChangeDate :" + selectedDate);
+            String selectedYear = checkLeapYear(pickerDp.getYear());
+            yearTxt.setText(selectedYear);
+        });
     }
-    public String getSelectedDate(){
-        int day = pickerDp .getDayOfMonth();
-        int month = pickerDp.getMonth()+1;
+
+    public String getSelectedDate() {
+        int day = pickerDp.getDayOfMonth();
+        int month = pickerDp.getMonth() + 1;
         int year = pickerDp.getYear();
-        String selectedDate = createDate(day,month,year);
+        String selectedDate = createDate(day, month, year);
         return selectedDate;
     }
 
-    public String createDate(int day,int month,int year){
+    public String createDate(int day, int month, int year) {
         return day + "/" + month + " / " + year;
 
+    }
+
+    public String checkLeapYear(int year) {
+        if (year % 4 == 0) {
+            return year + " is a leap year";
+        } else {
+            return year + " is not a leap year ";
+        }
     }
 }
 
